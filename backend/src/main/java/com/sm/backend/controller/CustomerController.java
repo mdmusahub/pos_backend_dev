b1fd3767ab80b37cd1991564d1d565e7804a5c12
ZaidAli19 <playzzzaid@gmail.com>
2025-06-24 14:13:00 +0530

    created customer entity.

--- backend/src/main/java/com/sm/backend/controller/CustomerController.java
+++ backend/src/main/java/com/sm/backend/controller/CustomerController.java
@@ -3,12 +3,15 @@ package com.sm.backend.controller;
 
 import com.sm.backend.exceptionalHandling.ResourceNotFoundException;
 import com.sm.backend.model.Customer;
+import com.sm.backend.request.CustomerRequest;
 import com.sm.backend.response.CustomerResponse;
 import com.sm.backend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -30,5 +33,30 @@ try {
     throw new ResourceNotFoundException(e.getMessage());
    }
     }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<CustomerResponse>> getAll(){
+    try{
+        return new ResponseEntity<>(customerService.getAll(),HttpStatus.OK);
+    } catch (Exception e) {
+        throw new ResourceNotFoundException("cannot retrieve customers");
+    }
+    }
+@PutMapping("/update/{id}")
+    public void updateCustomer(@RequestBody CustomerRequest request,@PathVariable Long id){
+    try {
+        customerService.update(request,id);
+    } catch (Exception e) {
+        throw new ResourceNotFoundException(e.getMessage());
+    }
+}
+@DeleteMapping("/delete/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+    try {
+        customerService.delete(id);
+} catch (Exception e) {
+        throw new ResourceNotFoundException(e.getMessage());
+    }
+}
+
 
 }
--- backend/src/main/java/com/sm/backend/serviceImpl/CustomerServiceImpl.java
+++ backend/src/main/java/com/sm/backend/serviceImpl/CustomerServiceImpl.java
@@ -3,13 +3,18 @@ package com.sm.backend.serviceImpl;
 import com.sm.backend.exceptionalHandling.ResourceNotFoundException;
 import com.sm.backend.model.Customer;
 import com.sm.backend.repository.CustomerRepository;
+import com.sm.backend.request.CustomerRequest;
 import com.sm.backend.response.CustomerResponse;
 import com.sm.backend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
-@Autowired
+
+    @Autowired
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
@@ -18,6 +23,31 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerResponse getById(Long id) {
         Customer customer = customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("invalid customerId"));
-return new CustomerResponse(customer);
+        return new CustomerResponse(customer);
+    }
+
+    @Override
+    public List<CustomerResponse> getAll() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream().map(CustomerResponse::new).toList();
+    }
+
+    @Override
+    public void update(CustomerRequest request, Long id) {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("invalid id"));
+        if (request.getPhoneNumber() != null) {
+            customer.setPhoneNumber(request.getPhoneNumber());
+        }
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        try {
+        customerRepository.delete(customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("invalid id")));
+    } catch (Exception e) {
+            throw new ResourceNotFoundException("This customer is involved in an order.");
+        }
     }
 }
